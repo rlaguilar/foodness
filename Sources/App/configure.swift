ce3579ebbe8e74ea68d44454fca30a4c09289643
Reynaldo Aguilar <rlac1990@gmail.com>
2021-02-07 00:23:01 +1100

    add different db configuration

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -6,15 +6,25 @@ import Vapor
 public func configure(_ app: Application) throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
-
-    app.databases.use(.postgres(
-        hostname: Environment.get("DATABASE_HOST") ?? "localhost",
-        port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? PostgresConfiguration.ianaPortNumber,
-        username: Environment.get("DATABASE_USERNAME") ?? "reynaldoaguilar",
-        password: Environment.get("DATABASE_PASSWORD") ?? "",
-        database: Environment.get("DATABASE_NAME") ?? "foodness"
-    ), as: .psql)
-
+    
+    switch app.environment {
+    case .production:
+        app.databases.use(.postgres(
+            hostname: Environment.get("DATABASE_HOST") ?? "localhost",
+            port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? PostgresConfiguration.ianaPortNumber,
+            username: Environment.get("DATABASE_USERNAME") ?? "foodness",
+            password: Environment.get("DATABASE_PASSWORD") ?? "qwerty",
+            database: Environment.get("DATABASE_NAME") ?? "foodness"
+        ), as: .psql)
+    default:
+        app.databases.use(.postgres(
+            hostname: Environment.get("DATABASE_HOST") ?? "localhost",
+            port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? PostgresConfiguration.ianaPortNumber,
+            username: Environment.get("DATABASE_USERNAME") ?? "reynaldoaguilar",
+            password: Environment.get("DATABASE_PASSWORD") ?? "",
+            database: Environment.get("DATABASE_NAME") ?? "foodness"
+        ), as: .psql)
+    }
     
     app.migrations.add(CreateRecipe())
     app.migrations.add(CreateIngredient())
